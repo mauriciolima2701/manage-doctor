14a21f6791ba3e99b49d4c2e35bf1113f928119d
mauriciolima2701 <mauricio2701@gmail.com>
2025-05-28 00:56:27 -0300

    chore: add better auth setup

---
+++ src/app/authentication/components/login-form.tsx
@@ -0,0 +1,75 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const loginSchema = z.object({
+    email: z.string().trim().min(1, { message: "Email é obrigatório" }).email({ message: "Email inválido" }),
+    password: z.string().trim().min(8, { message: "Senha obrigatória e deve conter pelo menos 8 caracteres" }),
+})
+
+export function LoginForm() {
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof loginSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Card>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <CardHeader>
+                        <CardTitle>Login</CardTitle>
+                        <CardDescription>
+                            Faça login para continuar
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Digite seu email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Senha</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Digite sua senha" {...field} type="password" />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                    <CardFooter>
+                        <Button type="submit" className="w-full">Entrar</Button>
+                    </CardFooter>
+                </form>
+            </Form>
+        </Card>
+    )
+}
--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import { LoginForm } from "./components/login-form";
 import { SignUpForm } from "./components/sign-up-form";
 
 export default function AuthenticationPage() {
@@ -15,23 +14,10 @@ export default function AuthenticationPage() {
                     <TabsTrigger value="register">Criar conta</TabsTrigger>
                 </TabsList>
                 <TabsContent value="login">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Login</CardTitle>
-                            <CardDescription>
-                                Faça login para continuar.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className="space-y-2">
-                            <SignUpForm />
-                        </CardContent>
-                        <CardFooter>
-                            <Button>Entrar</Button>
-                        </CardFooter>
-                    </Card>
+                    <LoginForm />
                 </TabsContent>
                 <TabsContent value="register">
-
+                    <SignUpForm />
                 </TabsContent>
             </Tabs>
         </div>
