cf10904fd8e0ee77082d8ff17cec9033d3038b6b
mauriciolima2701 <mauricio2701@gmail.com>
2025-05-29 18:11:26 -0300

    feat: add custom error message for existing user

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -36,6 +37,13 @@ export function SignUpForm() {
         }, {
             onSuccess: () => {
                 router.push("/dashboard");
+            },
+            onError: (ctx) => {
+                if (ctx.error.code === "USER_ALREADY_EXISTS") {
+                    toast.error("E-mail já cadastrado.");
+                    return
+                }
+                toast.error("Erro ao criar conta.");
             }
         })
     }
